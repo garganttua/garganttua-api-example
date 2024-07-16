dccc381475c277259e4a154d59a063e7d257b4be
Colombet Jérémy <gitlab@citech.fr>
2024-07-16 16:20:37 +0200

    ,h,h

--- src/main/java/com/garganttua/api/example/GarganttuaAPIExampleBackendConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.garganttua.api.example;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GarganttuaAPIExampleBackendConfiguration {
-	
-
-
-}
